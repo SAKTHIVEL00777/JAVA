8fe9110bb83ca03b6069c547ad08c178e01cc43b
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-10-19 11:30:21 +0530

    parent to child

---
+++ Telephone.java
@@ -0,0 +1,19 @@
+class Telephone{
+
+int price=1500;
+
+String name="phone";
+
+public void call(){
+
+  System.out.println("Calling Facility");
+
+}
+public void receive_call(){
+
+  System.out.println("Receiving Call Facility"); 
+
+}
+
+
+}
