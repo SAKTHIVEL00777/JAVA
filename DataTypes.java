e628e9d45e576a721646cc41740ea3f8be566323
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-11-30 17:11:57 +0530

    datatypes

---
+++ DataTypes.java
@@ -0,0 +1,29 @@
+public class DataTypes{
+
+
+public static void main(String args[]){
+
+   int no=100;
+   byte b=125;
+   short s=30521;
+   boolean bo=true;
+   float f=456766754.745f;
+   double d=8654567.86d;
+   long set=9688764121L;
+   char c='s';
+   String ss="collgge";
+  
+    System.out.println(no);
+    System.out.println(b);
+    System.out.println(s);
+    System.out.println(bo);
+    System.out.println(f);
+    System.out.println(d);
+    System.out.println(set);
+    System.out.println(c);
+    System.out.println(ss);
+  
+  
+
+}
+}
