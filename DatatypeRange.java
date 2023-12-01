805cefef1fa10f422b1d07fd7a7695623e23a72f
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-12-01 13:16:08 +0530

    datatyperanges

---
+++ DatatypeRange.java
@@ -0,0 +1,49 @@
+public class DatatypeRange{
+
+
+public static void main(String args[]){
+
+int i1=Integer.MIN_VALUE;
+int i2=Integer.MAX_VALUE;
+
+float f1=Float.MIN_VALUE;
+float f2=Float.MAX_VALUE;
+
+double d1=Double.MIN_VALUE;
+double d2=Double.MAX_VALUE;
+
+long l1=Long.MIN_VALUE;
+long l2=Long.MAX_VALUE;
+
+short s1=Short.MIN_VALUE;
+short s2=Short.MAX_VALUE;
+
+byte b1=Byte.MIN_VALUE;
+byte b2=Byte.MAX_VALUE;
+
+int c1=Character.MIN_VALUE;
+int c2=Character.MAX_VALUE;
+
+System.out.println("Integer Minimum Value Is" + i1);
+System.out.println("Integr Maximum Value Is" + i2);
+
+System.out.println("Float Minimum Value Is" + f1);
+System.out.println("Float Maximum Value Is" + f2);
+
+System.out.println("Double Minimum Value Is" + d1);
+System.out.println("Double Maximum Value Is" + d2);
+
+System.out.println("Long Minimum Value Is" + l1);
+System.out.println("Long Maximum Value Is" + l2);
+
+System.out.println("Short Minimum Value Is" + s1);
+System.out.println("Short Maximum Value Is" + s2);
+
+System.out.println("Byte Minimum Value Is" + b1);
+System.out.println("Byte Maximum Value Is" + b2);
+
+System.out.println("Character Minimum Value Is" + c1);
+System.out.println("Character  Maximum Value Is" + c2);
+}
+
+}
