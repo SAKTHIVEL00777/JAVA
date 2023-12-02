bcad7f65cbc13f06fb979955882b6547216f80dc
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-12-02 13:15:30 +0530

    datatypesize

---
+++ DatatypeSize.java
@@ -0,0 +1,22 @@
+public class DatatypeSize{
+
+
+public static void main(String args[]){
+
+System.out.println(Integer.SIZE);
+
+System.out.println(Float.SIZE);
+
+System.out.println(Long.SIZE);
+
+System.out.println(Double.SIZE);
+
+System.out.println(Character.SIZE);
+
+System.out.println(Short.SIZE);
+
+System.out.println(Byte.SIZE);
+
+
+}
+}
