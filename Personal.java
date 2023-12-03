1cbc9c40c5322ee96edec1599c522821d5e63717
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-12-03 15:50:55 +0530

    biodata

---
+++ Personal.java
@@ -0,0 +1,26 @@
+public class Personal{
+
+public static void main(String args[]){
+
+String name="SAKTHIVEL";
+String email="sakhivelr.techymeet@gmail.com";
+long mobileno=9688764121L;
+String address="2/128A,Thuraiyur(tk),Trichy(dt)";
+String  dob="04/07/1997";
+String aadhaar="4565 7632 8769";
+int pin=621002;
+int csary=18000;
+long esary=25000;
+
+System.out.println("NAME:"+ name);
+System.out.println("EMAIL:"+email);
+System.out.println("MOBILE NO:"+ mobileno);
+System.out.println("ADDRESS:"+ address);
+System.out.println("DOB:"+ dob);
+System.out.println("AADHAAR:"+ aadhaar);
+System.out.println("PINCODE:"+ pin);
+System.out.println("CSALARY:"+ csary);
+System.out.println("ESALARY:"+ esary);
+}
+
+}
