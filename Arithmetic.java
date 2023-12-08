889dedd7e52d5a1e43eb0749a5f7035808bc8846
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-12-08 15:26:25 +0530

    + - * /  %

---
+++ Arithmetic.java
@@ -0,0 +1,18 @@
+public class Arithmetic{
+
+public static void main(String args[]){
+
+int x=15;
+int y=3;
+
+//int z=x+y;
+
+System.out.println("ARITHMETIC OPERATION");
+System.out.println("ADD:"+ (x+y));
+System.out.println("SUB:"+ (x-y));
+System.out.println("MULTI:"+ (x*y));
+System.out.println("DIV:"+ (x/y));
+System.out.println("MOD:"+ (x%y));
+
+}
+}
