05bf0237a60a8c06b501d6da5eb42763374c7e51
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-12-09 18:08:15 +0530

    ++  --

---
+++ Unary.java
@@ -0,0 +1,27 @@
+public class Unary{
+
+public static void main(String args[]){
+int b=22;
+
+System.out.println("UNARY OPERATION");
+System.out.println("PRE & POST INCREMENT");
+System.out.println(++b);
+System.out.println("Value of Pre:"+ b);
+
+System.out.println(b++);
+System.out.println("Value of Post:"+ b);
+
+System.out.println("==========================");
+int c=33;
+
+System.out.println("PRE & POST DECREMENT");
+System.out.println(--c);
+System.out.println("Value of Pre:"+ c);
+
+System.out.println(c--);
+System.out.println("Valie Of Post:"+ c);
+
+}
+
+
+}
