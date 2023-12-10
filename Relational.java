f87e4f593ca45d1795ef0e4237da07c2c2debad1
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-12-10 18:32:16 +0530

    ==  !=  >  <   >=  <=

---
+++ Relational.java
@@ -0,0 +1,23 @@
+public class Relational{
+
+
+public static void main(String args[]){
+
+int r=13;
+int s=8;
+
+System.out.println("RELATIONAL OPERATION");
+
+System.out.println(r==s);
+System.out.println(r!=s);
+System.out.println(r>s);
+System.out.println(r<s);
+System.out.println(r>=s);
+System.out.println(r<=s);
+
+
+
+}
+
+
+}
