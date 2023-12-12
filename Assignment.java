9cbed3cabca35adbad9a48e6f56d07b9ec086331
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-12-12 14:24:43 +0530

    s+3,s_3,s*3,s/3,s%3

---
+++ Assignment.java
@@ -0,0 +1,25 @@
+public class Assignment{
+
+public static void main(String args[]){
+
+int s=15;
+
+System.out.println("ASSIGNMENT OPERATOR");
+s+=3;   //s=s+3;
+System.out.println("ADD:"+ s);
+
+s-=3;  //18=18-3=15;
+System.out.println("SUB:"+ s);
+
+s*=3;  //15=15*3=45;
+System.out.println("MULTI:"+ s);
+
+s/=3; //45=45/3=15;
+System.out.println("DIV:"+ s);
+
+s%=3; //15=15%3=0;
+System.out.println("MOD:"+ s);
+
+}
+
+}
