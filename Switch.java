8f186fd87f641bac1b33fcf653b8b11a4e122370
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-12-13 14:16:45 +0530

    case condition

---
+++ Switch.java
@@ -0,0 +1,35 @@
+public class Switch{
+
+public static void main(String args[]){
+
+String name="sakthi";
+
+switch(name){
+
+case "kavin":
+System.out.println("KAVIN BLOCK");
+break;
+
+case "sakthi":
+System.out.println("SAKTHI BLOCK");
+break;
+
+case "kumar":
+ System.out.println("KUMAR BLOCK");
+break;
+
+case "vel":
+ System.out.println("VEL BLOCK");
+break;
+
+
+
+default:
+ System.out.println("DEFAULT BLOCK");
+
+}
+
+}
+
+
+}
