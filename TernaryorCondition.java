92073acfdad4c1142acf07c07e4e102df4fa90e6
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-12-13 14:13:03 +0530

    variable=(condition)?exp1:exp2

---
+++ TernaryorCondition.java
@@ -0,0 +1,18 @@
+public class TernaryorCondition{
+
+public static void main(String args[]){
+
+int m=7;
+int n=4;
+
+System.out.println("TERNARY OR CONDITION OPERATOR");
+
+int max=(m>n)?m:n;
+System.out.println("Condition true:"+ max);
+
+int min=(m<n)?m:n;
+System.out.println("Condition false:"+ min);
+
+}
+
+}
