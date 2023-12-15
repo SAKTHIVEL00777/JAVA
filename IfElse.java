9c19cddf0d5947f6c765414735162bc6ba54894c
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-12-15 12:13:26 +0530

    condition true if & condition false else

---
+++ IfElse.java
@@ -0,0 +1,19 @@
+public class IfElse{
+
+public static void main(String args[]){
+
+int age=21;
+
+
+if(age>20)
+{
+ System.out.println("you are eligible for voting");
+}
+else if(age<18)
+{
+  System.out.println("you are not eligible for voting");
+}
+
+}
+
+}
