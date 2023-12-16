07282fa0ff6e622d5fe9ce0f8f47ffa53a23c788
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-12-16 20:00:06 +0530

    true & false & both are equal

---
+++ IfElseIf.java
@@ -0,0 +1,25 @@
+public class IfElseIf{
+
+public static void main(String args[]){
+
+int a=20;
+int b=2;
+
+if(a<b)
+{
+ System.out.println("IF block is call");
+}
+
+else if(b<a)
+{
+  System.out.println("ELSE IF block is call");
+}
+
+else if(b==a)
+{
+  System.out.println("Both are Equal");
+}
+
+}
+
+}
