b09c14a234554a00fef29557a977208ee8b4250c
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-12-17 20:44:08 +0530

    nested condition

---
+++ NestedIf.java
@@ -0,0 +1,28 @@
+public class NestedIf{
+
+public static void main(String args[]){
+
+int science=95;
+int social=93;
+int maths=91;
+
+if(science>92)
+{
+  System.out.println("SCIENCE MARK BLOCK");
+
+if(social>91)
+{
+  System.out.println("SOCIAL MARK BLOCK");
+}
+
+if(maths<89)
+{
+  System.out.println("MATHS MARK BLOCK");
+}
+
+}
+
+
+}
+
+}
