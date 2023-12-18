78ed93e0265165e8d3f4d2e9d9888d328fa96a4f
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-12-18 12:47:50 +0530

    for(initilization  condition  incremet)

---
+++ ForLoop.java
@@ -0,0 +1,16 @@
+public class ForLoop{
+
+public static void main(String args[]){
+
+//int i=7;===>initilization
+//i<=7====>condition
+//i++=====>increment     i--====>decrement
+
+for(int i=1;i<=7;i++){
+  
+  System.out.println(i);
+
+ }
+}
+
+}
