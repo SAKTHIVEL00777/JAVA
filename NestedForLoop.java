480bf20543359a38a90f5a5a6f62a1012dd38961
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-12-24 11:49:16 +0530

    nested for

---
+++ NestedForLoop.java
@@ -0,0 +1,17 @@
+public class NestedForLoop{
+
+public static void main(String args[]){
+
+for(int m=1;m<=2;m++)
+{
+  
+   for(int n=1;n<=2;n++)
+   {
+        System.out.println(m+""+n);
+        //System.out.println("m:"+ m +",n:"+ n);
+      }
+}
+
+}
+
+}
