0b8bc03df25c3374557a486e658b33c929a96780
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-12-28 12:20:03 +0530

    without parameter with return

---
+++ MethodTwo.java
@@ -0,0 +1,22 @@
+public class MethodTwo{
+
+public static void main(String args[]){
+
+ MethodTwo mt=new MethodTwo();
+ int s= mt.sub();
+  System.out.println("Without Parameter With Return");
+ System.out.println(s);
+
+
+}
+public int sub(){
+
+int a=7;
+int b=4;
+int c=a-b;
+
+return c;
+
+}
+
+}
