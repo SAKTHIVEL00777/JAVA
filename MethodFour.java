1e65824b05283cf9b84f08f1a2be684cfa7bcf26
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-12-30 11:55:40 +0530

    without parameter without return

---
+++ MethodFour.java
@@ -0,0 +1,15 @@
+public class MethodFour{
+
+public static void main(String args[]){
+
+  MethodFour mf=new MethodFour();
+  mf.sms();
+}
+
+public void sms(){
+
+ System.out.println("Without Parameter Without Return");
+ System.out.println("WELCOME TO ALL METHODS");
+}
+
+}
