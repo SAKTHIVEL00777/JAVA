f46277350df5fb2070e85b1d762581160d3aaad2
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-01-03 13:10:12 +0530

    string concat & bodmas

---
+++ StrConcat.java
@@ -0,0 +1,22 @@
+public class StrConcat{
+
+public static void main(String args[]){
+
+String val="SEVEN"+"STAR"+"CRICKET"+"CLUB";
+
+System.out.println("STRING CONCADINATION");
+System.out.println(val);
+System.out.println("=====================");
+
+String str=50+20+"MESSI"+20+20;
+System.out.println("BODMAS RULE");
+System.out.println(str);
+System.out.println("=====================");
+
+String ref=30*30+"NEYMAR"+35*35;
+System.out.println("BODMAS RULE");
+System.out.println(ref);
+
+}
+
+}
