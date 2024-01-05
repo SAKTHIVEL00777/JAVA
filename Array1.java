dced173d5e2db0bed808df14aff992a3310be43f
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-01-05 13:17:58 +0530

    string literal method

---
+++ Array1.java
@@ -0,0 +1,15 @@
+public class Array1{
+
+public static void main(String args[]){
+
+ int arr[]={5,10,15,20,25,30,35,40,45};
+
+for(int i=3;i<8;i++){
+
+System.out.println("ARRAY VALUE IS:"+ arr[i]); 
+
+}
+
+}
+
+}
