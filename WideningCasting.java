2c24f04d0df27a6eab145af79286853d486418e5
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-01-09 13:13:04 +0530

    small to large data type

---
+++ WideningCasting.java
@@ -0,0 +1,24 @@
+public class WideningCasting{
+
+public static void main(String args[]){
+
+byte b=27;
+short s=b;
+System.out.println("BYTE VALUE:"+ b);
+System.out.println("SHORT VALUE:"+ s);
+System.out.println("============================");
+
+int i=100;
+long l=i;
+System.out.println("INT VALUE:"+ i);
+System.out.println("LONG VALUE:"+ l);
+System.out.println("============================");
+
+float f=124;
+double d=f;
+System.out.println("FLOAT VALUE:"+ f);
+System.out.println("DOUBLE VALUE:"+ d);
+
+}
+
+}
