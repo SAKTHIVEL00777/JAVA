619e85fca4adf9a20943e7ca1dcba5ce1a01f034
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-01-11 13:16:37 +0530

    1st static block declare the method call

---
+++ StaticBlock.java
@@ -0,0 +1,15 @@
+public class StaticBlock{
+
+static{
+  System.out.println("STATIC 2nd BLOCK");
+}
+
+public static void main(String args[]){
+
+ System.out.println("MAIN BLOCK");
+}
+static{
+  System.out.println("STATIC 1st BLOCK");
+}
+
+}
