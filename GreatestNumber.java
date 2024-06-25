498c60d46b2dc52bd71c99aafb4cd6e4aeb2aa1c
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-06-25 16:51:47 +0530

    if condition

---
+++ GreatestNumber.java
@@ -0,0 +1,17 @@
+public class GreatestNumber{
+
+public static void main(String args[]){
+
+int x=71;
+int y=65;
+
+if(x>y){
+  System.out.println("GREATEST NUMBER IS:"+ x);
+}
+else{
+System.out.println("condition false");
+}
+
+}
+
+}
