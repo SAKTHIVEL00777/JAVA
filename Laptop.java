67a53685c637d4bee1b4d88fb43581e869628f3a
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-06-28 22:00:34 +0530

    method of sports

---
+++ Laptop.java
@@ -0,0 +1,39 @@
+public class Laptop{
+
+public static void main(String args[]){
+
+   Laptop lap=new Laptop();
+      lap.cricket();
+      lap.hocky();
+      lap.foot();
+      lap.volly();
+      lap.chess();
+}
+
+public void cricket(){
+
+  System.out.println("1.CRICKET CLUB"); 
+
+}
+public void hocky(){
+
+  System.out.println("2.HOCKY CLUB"); 
+
+}
+public void foot(){
+
+  System.out.println("3.FOOTBALL CLUB"); 
+
+}
+public void volly(){
+
+  System.out.println("4.VOLLYBALL CLUB"); 
+
+}
+public void chess(){
+
+  System.out.println("5.CHESS CLUB"); 
+
+}
+
+}
