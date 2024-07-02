4df047321bf43cf70412bcafa72623edad734005
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-07-02 17:39:00 +0530

    sum of 10 Natural Nos

---
+++ SumNatural.java
@@ -0,0 +1,14 @@
+public class SumNatural{
+
+public static void main(String args[]){
+
+ int sum=0;
+ for(int i=1;i<=10;i++){
+
+  sum+=i;
+}
+   System.out.println("SUM OF 1st 10 NATURAL NUMBERS:"+sum);
+  
+}
+
+}
