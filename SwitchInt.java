df4339e9f38d3b6ca6fd4c18a26b703e519cc5e1
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-07-03 12:25:01 +0530

    switch condition  & int method call

---
+++ SwitchInt.java
@@ -0,0 +1,60 @@
+public class SwitchInt{
+
+public static void main(String args[]){
+
+int month=7;
+System.out.println("SWITCH INT METHOD:"+ month);
+switch(month){
+
+case 1:
+  System.out.println("JANUARY");
+break;
+
+case 2:
+  System.out.println("FEBRUARY");
+break;
+
+case 3:
+  System.out.println("MARCH");
+break;
+
+case 4:
+  System.out.println("APRIL");
+break;
+
+case 5:
+  System.out.println("MAY");
+break;
+case 6:
+  System.out.println("JUNE");
+break;
+
+case 7:
+  System.out.println("JULY");
+break;
+
+case 8:
+  System.out.println("AUGUST");
+break;
+
+case 9:
+  System.out.println("SEPTEMBER");
+break;
+
+case 10:
+  System.out.println("OCTOBER");
+break;
+
+case 11:
+  System.out.println("NOVEMBER");
+break;
+
+case 12:
+  System.out.println("DECEMBER");
+break;
+
+}
+
+}
+
+}
