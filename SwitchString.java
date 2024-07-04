ab7e4310613c44b2efc8b7bfde83c48219922c96
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-07-04 17:59:54 +0530

    switch condition  & string method call

---
+++ SwitchString.java
@@ -0,0 +1,25 @@
+public class SwitchString{
+
+public static void main(String args[]){
+
+String food="chicken";
+System.out.println("SWITCH STRING METHOD:"+ food);
+switch(food){
+
+case "veg":
+System.out.println("VEG BRIYANI");
+break;
+
+case "chicken":
+System.out.println("CHICKEN BRIYANI");
+break;
+
+case "mutton":
+System.out.println("MUTTON BRIYANI");
+break;
+}
+
+
+}
+
+}
