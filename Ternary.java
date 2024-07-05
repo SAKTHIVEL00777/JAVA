f3b2a44d273c25753c91c5b8d40ac835421b414f
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-07-05 16:52:01 +0530

    ternary operator

---
+++ Ternary.java
@@ -0,0 +1,18 @@
+public class Ternary{
+
+public static void main(String args[]){
+
+int x=777;
+int y=712;
+
+System.out.println("TERNARY OR CONDITION OPERATOR");
+
+int max=(x>y)?x:y;
+System.out.println("1st VALUE IS GREATERTHAN:"+ max);
+
+int min=(x<y)?x:y;
+System.out.println("2nd VALUE IS LESSTHAN:"+ min);
+
+}
+
+}
