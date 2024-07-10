4a34460d639f26cb9896b311f2d799f314f5f37a
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-07-10 20:11:21 +0530

    sorting

---
+++ ArraySort.java
@@ -0,0 +1,15 @@
+public class ArraySort{
+
+
+public static void main(String args[]){
+
+ int arr[]={2,5,1,88,47,21,49,100,99,67}; 
+
+  for(int i=0;i<arr.length;i++){
+   
+   System.out.print(arr[i]+" ");
+}
+
+}
+
+}
