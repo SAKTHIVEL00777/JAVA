e727c65f059d13ce130259d0c067e17e35c3e10c
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-07-26 12:21:58 +0530

    str val checking for if & else condition

---
+++ EmptyOrNOtEmpty.java
@@ -0,0 +1,18 @@
+public class EmptyOrNOtEmpty{
+
+public static void main(String args[]){
+
+String val="";
+
+if(val.isEmpty()){
+
+   System.out.println("Empty");
+ 
+}
+ else{
+      System.out.println("Not Empty");
+   }
+
+}
+
+}
