7941bf0221955e4c4a959084334a830b590ae668
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-07-29 12:59:09 +0530

    space remove

---
+++ RemoveAll.java
@@ -0,0 +1,14 @@
+public class RemoveAll{
+
+public static void main(String args[]){
+
+String name=" Welcome To Techymeet";
+
+String space=name.replace(" ","");
+
+System.out.println(space);
+
+}
+	
+
+}
