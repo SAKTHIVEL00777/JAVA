02c9a5d6567e9a2c33b7ac1d9bab8a5d2f3e7063
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-08-01 11:06:26 +0530

    reverse string

---
+++ Reverse1.java
@@ -0,0 +1,16 @@
+public class Reverse1{
+
+public static void main(String args[]){
+
+String name="Techymeet";
+
+for(int i=name.length()-1;i>=0;i--){
+
+System.out.print(name.charAt(i));
+
+}
+
+
+}
+
+}
