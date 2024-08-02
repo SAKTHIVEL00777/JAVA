ce7da566904e5ab39eb55281437159e0c7f639b2
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-08-02 17:49:02 +0530

    string reverse

---
+++ Reverse2.java
@@ -0,0 +1,14 @@
+public class Reverse2{
+
+public static void main(String args[])
+{
+
+StringBuffer sb=new StringBuffer("Techymeet");
+System.out.println("Before String:"+ sb);
+
+sb.reverse();
+System.out.println("After String:"+ sb);
+
+}
+
+}
