e98a817acefc72a7be3cf6a758d1693ec37b6c86
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-08-05 21:12:47 +0530

    check for string word

---
+++ SuccesOrFailed.java
@@ -0,0 +1,18 @@
+public class SuccesOrFailed{
+
+public static void main(String args[]){
+
+String str="Success";
+     
+if(str.equals(str)){
+
+  System.out.println("Success");
+}
+
+else{
+    	 System.out.println("Failed");
+     }
+
+}
+
+}
