92823438f68f02beeedd579f3890720d8a4895e3
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-08-13 16:05:33 +0530

    boolean value change

--- DataTypes.java
+++ DataTypes.java
@@ -6,7 +6,7 @@ public static void main(String args[]){
    int no=100;
    byte b=125;
    short s=30521;
-   boolean bo=true;
+   boolean bo=false;
    float f=456766754.745f;
    double d=8654567.86d;
    long set=9688764121L;
